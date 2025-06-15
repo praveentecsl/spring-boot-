84ff4253fa1882063591a966e4f6543cf42b37b0
PraveenWE <praveentecsl@gmail.com>
2025-06-15 15:13:27 +0530

    display the genarated list of objects in json format

--- Test_4/demo_sunday/demo_sunday/src/main/java/com/example/demo_sunday/StudentRestController.java
+++ Test_4/demo_sunday/demo_sunday/src/main/java/com/example/demo_sunday/StudentRestController.java
@@ -1,18 +1,39 @@
 package com.example.demo_sunday;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/students")
 public class StudentRestController {
 
-    private ArrayList<Student> student = new ArrayList<>();
+    private ArrayList<Student> student ;
 
-    Student yasith = new Student("Yasith","yasith@gmail.com");
-    Student olitha = new Student("olitha","olitha@gmail.com");
-    Student praveen = new Student("praveen ","praveen@gmaol.com");
+    @PostConstruct
+    public void loadData(){
+        student = new ArrayList<>();
 
 
+        Student yasith = new Student("Yasith","yasith@gmail.com");
+        Student olitha = new Student("olitha","olitha@gmail.com");
+        Student praveen = new Student("praveen ","praveen@gmaol.com");
+        student.add(praveen);
+        student.add(olitha);
+        student.add(yasith);
 
-}
+
+    }
+
+    @GetMapping("/getAllStudents")
+    public ResponseEntity<List<Student>> getAllStudents(){
+        return ResponseEntity.ok(student);
+    }
+
+
+☻}
