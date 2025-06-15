d66dc6b30ffdf3f947ffeb2e6df9ebaa990fdc09
PraveenWE <praveentecsl@gmail.com>
2025-06-15 14:37:22 +0530

    create getter and setter

--- Test_4/demo_sunday/demo_sunday/src/main/java/com/example/demo_sunday/Student.java
+++ Test_4/demo_sunday/demo_sunday/src/main/java/com/example/demo_sunday/Student.java
@@ -1,15 +1,42 @@
 package com.example.demo_sunday;
 
 import java.security.PrivateKey;
+import java.util.Random;
 
 public class Student {
-    private  String Id;
+    private  Integer Id;
     private  String name;
     private  String email;
 
-    public Student(String id, String name, String email) {
+public Student( String name, String email) {
+    Random random = new Random();
+    Id = random.nextInt(0,100);
+    this.name = name;
+    this.email = email;
+}
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setId(Integer id) {
         Id = id;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 }
